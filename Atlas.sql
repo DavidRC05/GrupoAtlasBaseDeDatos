0f8dfb1b120cccef403a98b927cfb9e85dfa719f
DavidRC05 <>
2023-07-16 13:20:56 -0600

    Creacion Script

---
+++ Atlas.sql
@@ -0,0 +1,56 @@
+SET SERVEROUTPUT ON;
+
+/*
+    
+    SCRIPT BASE DE DATOS CLINIC CARE
+    LENGUAJE DE BASE DE DATOS
+    GRUPO ATLAS
+    
+    INTEGRANTES:
+    Athuro Chaves Aguilar
+    Catalina Chaves Aguilar 
+    David Rodriguez Castillo
+    Mathias Jimenes Prendas
+    
+*/
+
+--Creacion de Tablas
+
+
+
+--Insercion a Tablas
+
+
+
+--Restricciones de Tablas
+
+
+
+--Creacion de Usuarios
+
+
+
+--25 Procedimientos Almacenados
+
+
+
+--10 Vistas
+
+
+
+--15 Funciones
+
+
+
+--10 Paquetes
+
+
+
+--5 Triggers
+
+
+
+--15 Cursores
+
+
+
