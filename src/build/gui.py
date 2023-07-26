30b3f49e2b9512f6a5386ad4ebcfc050fffe8480
ArthCA026 <r2chaves@gmail.com>
2023-07-26 15:54:57 -0600

    Root Code del UI

--- src/build/gui.py
+++ src/build/gui.py
@@ -1,23 +1,22 @@
-
+#Imports
 from pathlib import Path
-
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 
-
+#Para rutas relativas
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"/Users/davidrodriguez/Desktop/COURSES/Ambiente Web/GrupoAtlasBaseDeDatos/GrupoAtlasBaseDeDatos/src/build/assets/frame0")
 
+#Esta ruta hay que cambiarla por cada compu
+ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\Arturo\Desktop\GeneratedCode\build\assets\frame0")
 
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
-
+#Creacion de la ventana
 window = Tk()
-
 window.geometry("960x600")
 window.configure(bg = "#F9F9F9")
 
-
+#Canvas para juntarle imagenes
 canvas = Canvas(
     window,
     bg = "#F9F9F9",
@@ -29,44 +28,6 @@ canvas = Canvas(
 )
 
 canvas.place(x = 0, y = 0)
-image_image_1 = PhotoImage(
-    file=relative_to_assets("image_1.png"))
-image_1 = canvas.create_image(
-    219.0,
-    278.0,
-    image=image_image_1
-)
 
-image_image_2 = PhotoImage(
-    file=relative_to_assets("image_2.png"))
-image_2 = canvas.create_image(
-    697.0,
-    277.0,
-    image=image_image_2
-)
-
-image_image_3 = PhotoImage(
-    file=relative_to_assets("image_3.png"))
-image_3 = canvas.create_image(
-    697.0,
-    446.0,
-    image=image_image_3
-)
-
-image_image_4 = PhotoImage(
-    file=relative_to_assets("image_4.png"))
-image_4 = canvas.create_image(
-    219.0,
-    448.0,
-    image=image_image_4
-)
-
-image_image_5 = PhotoImage(
-    file=relative_to_assets("image_5.png"))
-image_5 = canvas.create_image(
-    480.0,
-    71.0,
-    image=image_image_5
-)
 window.resizable(False, False)
 window.mainloop()
